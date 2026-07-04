db9ae9400b1030501d45170f4368ab397060f614
if-loop69420 <j.sztavi@pm.me>
2026-07-04 17:02:25 +0200

    init: initial commit. Setup workspaces and add dependencies

---
+++ uart_rng_receiver/lib/uart_rng_receiver.ex
@@ -0,0 +1,18 @@
+defmodule UartRngReceiver do
+  @moduledoc """
+  Documentation for `UartRngReceiver`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UartRngReceiver.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ uart_rng_receiver/test/uart_rng_receiver_test.exs
@@ -0,0 +1,8 @@
+defmodule UartRngReceiverTest do
+  use ExUnit.Case
+  doctest UartRngReceiver
+
+  test "greets the world" do
+    assert UartRngReceiver.hello() == :world
+  end
+end
